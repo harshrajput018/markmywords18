718cdabc89b283d0abc15d2b6ee4a35afe2d32ad
Harsh Rajput <harshrajput@Harshs-MacBook-Air.local>
2022-09-14 11:31:34 +0530

    19 commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app=express();
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname+"/public")))
-app.use(favicon(__dirname+"/public/favicon.ico"));
+app.use(favicon(__dirname+"/favicon.ico"));
 
 
 
