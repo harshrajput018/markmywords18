e68f5b999e0fe60cb762cf28c19012c7db619953
Harsh Rajput <harshrajput@Harshs-MacBook-Air.local>
2022-09-14 13:06:25 +0530

    add cloud database

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const deletenote = require('./routes/deletenote');
 const signup = require('./routes/signup');
 const search = require('./routes/search');
 
+const favicon=require('serve-favicon')
+
 const jwt = require('jsonwebtoken')
 
 
@@ -19,13 +21,15 @@ const jwt = require('jsonwebtoken')
 
 
 
-mongoose.connect('mongodb://localhost:27017').then(()=>{console.log('db has been connected')});
+mongoose.connect('mongodb+srv://harsh18:Harsh1827@cluster0.mzbhte5.mongodb.net/?retryWrites=true&w=majority').then(()=>{console.log('db has been connected')});
 
 const app=express();
 
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname+"/public")))
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 
 
